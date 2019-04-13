97dae730050d1f1ba837b025d9885e5ea4f3a90b
Godfrey Martinez <godmartinz@gmail.com>
2019-04-13 11:38:51 -0700

    adds initial project files

--- server/app.js
+++ server/app.js
@@ -1,10 +1,17 @@
 // import files and packages up here
-
-
+const morgan= require('morgan');
+const topSpot= require('./data.json');
 // create your express server below
-var app;
-
+const express= require('express');
+const app=express();
 // add your routes and middleware below
-
+app.use(morgan('dev'));
+app.use(express.static('public'));
+app.get('/', function(req, res){
+    res.status(200).send("Working..");
+});
+app.get('/data', function(req, res){
+    res.status(200).send(topSpot);
+});
 // finally export the express application
 module.exports = app;
